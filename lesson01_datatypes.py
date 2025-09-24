35e89ecd2a9f510ade7c1b0fc11fbdb0fd7ce553
Aarush544 <singhaarush620@gmail.com>
2025-09-24 16:18:10 +0000

    Create data types

---
+++ lesson01_datatypes.py
@@ -0,0 +1,45 @@
+# This is a comment
+
+# Data types
+
+# Strings:
+print('Hello World')
+print('Once upon a time \nand a very good time it was \nthere was a moocow')
+print('Name: \tBilly \nAge: \t14')                                                  
+
+# Integers:
+
+print(5 + 5 - 11)
+print(-42)
+print(32)
+
+
+# Float:
+
+print(4.0)
+print(4.0+6.0)
+print(4.0+6)
+
+#Boolean:
+
+print(True + False)
+print(5 + 1 == 10)
+print(True)
+print(1 == 2)
+
+
+
+print(type('Hello'))
+print(type(5))
+print(type(3.1))
+print(type(1 == 1))
+
+
+
+
+
+
+
+
+
+
