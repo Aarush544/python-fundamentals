ecf78e6b01c645fabcdcbf04320a4d71e874e1c4
Aarush544 <singhaarush620@gmail.com>
2025-09-26 17:47:32 +0000

    made lesson02

---
+++ lesson02_variables.py
@@ -0,0 +1,62 @@
+password = "\t8374aarush"
+
+email = "\taarush@gmail.com"
+
+temperature = 72.5
+
+print("Password: ", password)
+print("email: ", email)
+
+
+enabled = False
+is_complete = True
+
+print(is_complete)
+
+x = 3.15
+y = 7
+print(x + y)
+    
+
+
+count = 10
+print(count)
+count_down = count-1
+print(count_down)
+
+
+Name = "Radia Perlman"
+Age = 34
+Job = "Networking Engineer"
+
+print(f"{Name} is a {Age} year old {Job}")
+
+
+
+count = 10; addition_ammount = 5
+count = count + addition_ammount
+print(count)
+
+
+
+x = 4 
+y = "Hello"
+# temporary = x
+# x = y
+# y = temporary
+
+x, y = y, x
+print(x, y)
+
+
+
+is_raining = False
+is_raining = True
+
+print(f"Is it raining?\n{is_raining}") 
+
+
+
+
+
+
