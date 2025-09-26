cf847bf215dc1003076fb1f81a09f3fd9ac5b6f0
Aarush544 <singhaarush620@gmail.com>
2025-09-26 17:48:03 +0000

    changed data types

---
+++ lesson03_math.py
@@ -0,0 +1,13 @@
+add = 7 + 9
+print(f"Sum:\t{add}")
+subtract = 8-3
+print(f"Difference:\t{subtract}")
+
+multiply = 7 *2
+print(f"Produdct:\t{multiply} ")
+
+float_divide = 7/3
+print(f"Float Division:{float_divide}")
+
+integer_divide = 7//3
+print(f"Integer Division:\t{integer_divide}")
