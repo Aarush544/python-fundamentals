363ab67114e50625bb96163b700a810f0b282a97
Aarush544 <singhaarush620@gmail.com>
2025-09-26 18:21:45 +0000

    Edit

--- lesson03_math.py
+++ lesson03_math.py
@@ -1,13 +1,40 @@
 add = 7 + 9
-print(f"Sum:\t{add}")
+print(f"Sum:\t\t{add}")
 subtract = 8-3
 print(f"Difference:\t{subtract}")
 
-multiply = 7 *2
+multiply = 7 * 2
 print(f"Produdct:\t{multiply} ")
 
 float_divide = 7/3
-print(f"Float Division:{float_divide}")
+print(f"Float Division:\t{float_divide}")
 
 integer_divide = 7//3
-print(f"Integer Division:\t{integer_divide}")
+print(f"Floor Division:\t{integer_divide}")
+
+mod = 7%2
+print(f"Modulus:\t{mod}")
+
+
+exponent = 7 ** 2
+print(f"Exponent:\t{exponent}")
+
+result = 10-2 + 3
+
+
+
+#Challenges
+
+rectangle_area = 8*5
+print(rectangle_area)
+
+circle_area = 3.14 * 7 **2
+print(circle_area)
+
+total = 12.99*3 + 3.50*4
+print(total)
+
+
+num = input("Enter a number: ")
+is_even = num%2 == 0
+print(f"Is the number even: {is_even}")
