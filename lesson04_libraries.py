cc3512f4664be311fc2309d91ec9c7595607a03b
Aarush544 <singhaarush620@gmail.com>
2025-09-30 16:27:20 +0000

    random number

--- lesson04_libraries.py
+++ lesson04_libraries.py
@@ -6,5 +6,19 @@ print("Ceilling", math.ceil(7/2))
 print("Floor", math.floor(math.floor(7/2)))
 print("Pi", math.pi)
 
+               
+
+seed = 178.73
+
+step1 = seed/15
+
+seed2 = seed - 13
+
+step3 = seed * -2.65
+
+random_num = math.floor(step3)
+
+
+print(step3)
 
 
