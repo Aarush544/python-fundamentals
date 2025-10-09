36800f438bdb2d6a2c40bb1687def9d69d76f7db
Aarush544 <singhaarush620@gmail.com>
2025-10-09 17:42:59 +0000

    made strings.py

---
+++ lesson06_strings.py
@@ -0,0 +1,82 @@
+greeting = "Hello"
+name = "Aarush"
+
+print(f"Hello, {name}")
+
+message = greeting + ', ' + name
+print(f"Concatenated version: {message}")
+
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+
+word = 'pneumonoultramicroscopicsilicovolcanoconiosis' \
+
+print("First letter: ", word[0])
+
+print("Last letter: ", word[-1])
+
+print(word[8:13])
+print(word[13:24])
+print(word[30:37])
+
+print("Last 8 letters: ", word[-8:])
+
+
+print("Every second character: ", word[::2])
+
+print("Reversed: ", word[::-1])
+
+
+country = "Sweden"
+length_of_word = len(country)
+print(f"length: {length_of_word}")
+
+phrase = "sPonGEBoB Squarepants"
+print("Uppercase: ", phrase.upper())
+print("Lowercases: ", phrase.lower())
+print("Capitalized: ", phrase.capitalize())
+print("Title:", phrase.title())
+
+sentence = "I'm a goofy goober"
+new_sentence = sentence.replace('I\'m', 'You\'re')
+print(sentence)
+print(new_sentence)
+
+
+name = "Aarush"
+age = 14
+city = "Clinton"
+
+print(f"My name is {name}. I am {age} years old and I live in {city}")
+
+print(f"Next year I will be {age + 1}")
+
+# Challenge 1
+
+
+print(len(input("Enter your favorite quote: ")))
+
+#Challenge 2
+
+
+
+print(f"{input("Enter your last name: ")}," + f" {input("Enter your first name: ")}")
+
+
+#Challenge 3
+
+word = input("Enter a word: ")
+print(f"Uppercase: {word.upper()}\nLowercase: {word.lower()}\nBackwards: {word[::-1]} ")
+                        
+              
+
+
+
+
+
+
+
