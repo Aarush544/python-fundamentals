d78d59291979df131741eace74172a97e5fcf4cf
Aarush544 <singhaarush620@gmail.com>
2025-10-21 16:34:08 +0000

    lists

--- lesson08_lists.py
+++ lesson08_lists.py
@@ -10,131 +10,45 @@ shapes.append("hexagon")
 shapes.remove(1)
 print(shapes.pop())
 
+empty = []
+empty.append("Hello")
+print(empty)
+print(empty.index("Hello"))
 
+print(max(numbers))
+print(min(numbers))
+print(sum(numbers))
+numbers.sort()
 
+newlist = numbers
 
+print(newlist)
+print(numbers)
 
 
 
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
 
+]
 
 
+print(matrix[0][1])
 
 
+#Challenge 1
 
+# numbers = [1,2,3,4,5,6]
+# numbers[2] = input("Enter a number: ")
+# print(numbers)
 
+#Challenge 2
 
+shopping = []
+items = ["milk", "bread", "eggs"]
+shopping += items
+shopping.insert(1)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(shopping)
