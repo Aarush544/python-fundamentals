7625daa3de0a5ffaeb10b9f5b997e24c7d1eb33b
Aarush544 <singhaarush620@gmail.com>
2025-10-24 15:26:36 +0000

    loopin

---
+++ lesson09_loops.py
@@ -0,0 +1,34 @@
+#loops
+
+import time
+
+
+# num = 1
+# while num <= 10:
+#     print(num)
+#     num += 1
+
+# letters = ["a", "b", "c", "d", "e"]
+
+hello = "Hello! I am Aarush!\n"
+
+for i in hello:
+    print(f"{i}", end = "", flush= True)
+    time.sleep(0.2)
+
+# for i in range(1, 20, 2):
+#     print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
